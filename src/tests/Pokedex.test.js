5b9d6ed51cbfbead28ef084541ae08982a50ae29
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-05-25 18:55:45 -0300

    requisito 5.3.1 feito

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -25,4 +25,42 @@ describe('É exibido o próximo Pokémon quando o botão Próximo pokémon é cl
     const pokemon = getByText('Charmander');
     expect(pokemon).toBeInTheDocument();
   });
+  test('O picachu voltou', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const buttonNext = getByText('Próximo pokémon');
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    userEvent.click(buttonNext);
+    const pokemon = getByText('Pikachu');
+    expect(pokemon).toBeInTheDocument();
+  });
+});
+
+describe('Teste se é mostrado apenas um Pokémon por vez', () => {
+  test('Só um pokemon é mostrado', () => {
+    const { getAllByRole } = renderWithRouter(<App />);
+    const pokemonsInScreen = getAllByRole('img');
+    expect(pokemonsInScreen.length).toBe(1);
+  });
+});
+
+describe('Teste se a Pokédex tem os botões de filtro', () => {
+  test('A Pokédex deve circular somente pelos pokémons daquele tipo', () => {
+    const { getByText, getByRole } = renderWithRouter(<App />);
+    const typeFire = getByRole('button', { name: 'Fire' });
+    userEvent.click(typeFire);
+    expect(getByText('Charmander')).toBeInTheDocument();
+    const buttonNext = getByText('Próximo pokémon');
+    userEvent.click(buttonNext);
+    expect(getByText('Rapidash')).toBeInTheDocument();
+    const typeBug = getByRole('button', { name: 'Bug' });
+    userEvent.click(typeBug);
+    expect(getByText('Caterpie')).toBeInTheDocument();
+  });
 });
