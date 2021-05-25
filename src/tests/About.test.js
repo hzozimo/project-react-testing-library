b318202aa1c4bcfa70f396d3244c2f012ad632c4
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-05-25 16:24:21 -0300

    requisito 2.1 2.2 e 2.3 feitos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste se a página contém as informações sobre a Pokédex', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { getByRole } = renderWithRouter(<About />);
+    const aboutText = getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutText).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { getAllByText } = renderWithRouter(<About />);
+
+    const paragraphQuant = getAllByText(/Pokémons/);
+    expect(paragraphQuant.length).toBe(2);
+  });
+});
