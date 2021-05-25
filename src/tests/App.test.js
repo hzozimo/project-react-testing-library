e54b49229b6ffc20e1ca7fdc0637e18aff808dbe
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-05-25 15:16:12 -0300

    requisitos 1.1 e 1.2 feitos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
-});
+// test('renders a reading with the text `Pokédex`', () => {
+//   const { getByText } = renderWithRouter(<App />);
+//   const heading = getByText(/Pokédex/i);
+//   expect(heading).toBeInTheDocument();
+// });
 
 test('shows the Pokédex when the route is `/`', () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={ ['/'] }>
-      <App />
-    </MemoryRouter>,
-  );
-
+  const { getByText, history } = renderWithRouter(<App />);
+  const route = '/';
+  history.push(route);
   expect(getByText('Encountered pokémons')).toBeInTheDocument();
+});
+
+describe('o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  test('O primeiro link deve possuir o texto Home', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const route = '/';
+    history.push(route);
+    expect(getByText('Home')).toBeInTheDocument();
+  });
+
+  test('O segundo link deve possuir o texto About', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const route = '/';
+    history.push(route);
+    expect(getByText('About')).toBeInTheDocument();
+  });
+
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    const route = '/';
+    history.push(route);
+    expect(getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
 });
