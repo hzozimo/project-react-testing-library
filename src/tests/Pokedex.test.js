fd0993691810f821fc738c143416c02d2689f2a1
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-05-25 18:20:53 -0300

    requisitos 5.1 e 5.2.1 feitos

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import Pokedex from '../components/Pokedex';
+import App from '../App';
+
+test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+  const { getByRole } = renderWithRouter(<App />);
+  const pokemon = getByRole('heading', {
+    level: 2,
+    name: /Encountered pokémons/i,
+  });
+  expect(pokemon).toBeInTheDocument();
+});
+
+describe('É exibido o próximo Pokémon quando o botão Próximo pokémon é clicado', () => {
+  test('O botão deve conter o texto Próximo pokémon', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const buttonNext = getByText('Próximo pokémon');
+    expect(buttonNext).toBeInTheDocument();
+  });
+});
