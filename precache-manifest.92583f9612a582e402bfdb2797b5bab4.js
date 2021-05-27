b0820085997efcc25338fdd96a1f8d6af46c2fec
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-05-27 18:20:16 -0300

    Updates

---
+++ precache-manifest.92583f9612a582e402bfdb2797b5bab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88f5c7287a044f2eae59c0d444ba61da",
+    "url": "/project-react-testing-library/index.html"
+  },
+  {
+    "revision": "5b5702f1512fd56cfc26",
+    "url": "/project-react-testing-library/static/css/main.b9ae1f4b.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/project-react-testing-library/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project-react-testing-library/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5702f1512fd56cfc26",
+    "url": "/project-react-testing-library/static/js/main.9ed2391b.chunk.js"
+  },
+  {
+    "revision": "6ee681307ae1f61168b3",
+    "url": "/project-react-testing-library/static/js/runtime-main.1ff77a16.js"
+  }
+]);
